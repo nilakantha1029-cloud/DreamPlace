80f42b0fdc419497e4585aac82cb5402793b6f0f
Harsha <nilakantha1029@gmail.com>
2026-05-01 12:10:14 +0530

    fix python version for render

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,7 @@ app.config['SQLALCHEMY_DATABASE_URI']       = os.getenv(
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY']                = os.getenv('JWT_SECRET_KEY', 'jwt-secret-dreamplace-2025')
 app.config['JWT_ACCESS_TOKEN_EXPIRES']      = timedelta(hours=24)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
 # Extensions
 db      = SQLAlchemy(app)
